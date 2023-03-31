9c25364bcaa1d841be30e4ddf2a873e2f1a010ec
tuanhoang <tuanhanh19981998@gmail.com>
2023-03-31 23:20:36 +0700

    Init project

---
+++ DeadStripDemo/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  DeadStripDemo
+//
+//  Created by Tuan Hoang on 31/03/2023.
+//
+
+import UIKit
+import Pulse
+import PulseUI
+
+class ViewController: UIViewController {
+    let networkLogger = NetworkLogger()
+    let settingView = PulseUI.SettingsView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
